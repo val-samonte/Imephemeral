28a1b0731b35b8ac44d3f85703e942a5099a3c8e
Val <val.samonte@gmail.com>
2024-07-06 06:52:32 +0800

    Test nodepolyfills

--- app/frontend/vite.config.ts
+++ app/frontend/vite.config.ts
@@ -1,57 +1,81 @@
-import { createRequire } from 'module'
-import { resolve } from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
-const require = createRequire(import.meta.url)
-
-// Vite plugin to provide global variables
-const ProvidePlugin = (vars: any) => {
-  return {
-    name: 'vite-plugin-provide',
-    config(config: any) {
-      if (!config.define) {
-        config.define = {}
-      }
-      for (const key in vars) {
-        config.define[key] = vars[key]
-      }
-    },
-  }
-}
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  resolve: {
-    alias: {
-      http: resolve(require.resolve('stream-http')),
-      https: resolve(require.resolve('https-browserify')),
-      crypto: resolve(require.resolve('crypto-browserify')),
-      stream: resolve(require.resolve('stream-browserify')),
-      buffer: resolve(require.resolve('buffer/').replace(/index\.js$/, '')),
-      '@': resolve(__dirname, './src'),
-    },
-  },
-  base: '/', // Specify the base public path
-  build: {
-    // minify: false,
-    // terserOptions: {
-    //   compress: false,
-    //   mangle: false,
-    // },
-    rollupOptions: {
-      onwarn(warning, warn) {
-        if (/Failed to parse source map/.test(warning.message)) {
-          return
-        }
-        warn(warning)
-      },
-    },
-  },
   plugins: [
     react(),
-    ProvidePlugin({
-      Buffer: ['buffer', 'Buffer'],
-      // process: 'process/browser',
+    nodePolyfills({
+      include: ['path', 'stream', 'util'],
+      exclude: ['http'],
+      globals: {
+        Buffer: true,
+        global: true,
+        process: true,
+      },
+      overrides: {
+        fs: 'memfs',
+      },
+      protocolImports: true,
     }),
   ],
 })
+
+// import { createRequire } from 'module'
+// import { resolve } from 'path'
+// import { defineConfig } from 'vite'
+// import react from '@vitejs/plugin-react-swc'
+
+// const require = createRequire(import.meta.url)
+
+// // Vite plugin to provide global variables
+// const ProvidePlugin = (vars: any) => {
+//   return {
+//     name: 'vite-plugin-provide',
+//     config(config: any) {
+//       if (!config.define) {
+//         config.define = {}
+//       }
+//       for (const key in vars) {
+//         config.define[key] = vars[key]
+//       }
+//     },
+//   }
+// }
+
+// export default defineConfig({
+//   resolve: {
+//     alias: {
+//       http: resolve(require.resolve('stream-http')),
+//       https: resolve(require.resolve('https-browserify')),
+//       crypto: resolve(require.resolve('crypto-browserify')),
+//       stream: resolve(require.resolve('stream-browserify')),
+//       buffer: resolve(require.resolve('buffer/').replace(/index\.js$/, '')),
+//       '@': resolve(__dirname, './src'),
+//     },
+//   },
+//   base: '/', // Specify the base public path
+//   build: {
+//     // minify: false,
+//     // terserOptions: {
+//     //   compress: false,
+//     //   mangle: false,
+//     // },
+//     rollupOptions: {
+//       onwarn(warning, warn) {
+//         if (/Failed to parse source map/.test(warning.message)) {
+//           return
+//         }
+//         warn(warning)
+//       },
+//     },
+//   },
+//   plugins: [
+//     react(),
+//     ProvidePlugin({
+//       Buffer: ['buffer', 'Buffer'],
+//       // process: 'process/browser',
+//     }),
+//   ],
+// })
